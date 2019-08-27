aa0b41c4d758670266d6c6dd4c0c4abf51f3b61b
Benson <tumlivein@gmail.com>
2019-08-27 21:11:37 +0800

    Challenge

--- scripts/app.js
+++ scripts/app.js
@@ -66,6 +66,16 @@ var count = 0;
 var addOne = function addOne() {
   return console.log("addOne");
 };
+// Challenge
+// Make button "-1" -setup minusOne function and register - log 'minusOne'
+// Make reset button 'reset' - setup reset function - log 'reset'
+var minusOne = function minusOne() {
+  return console.log("minusOne");
+};
+var reset = function reset() {
+  return console.log("reset");
+};
+
 var templateTwo = React.createElement(
   "div",
   null,
@@ -79,8 +89,17 @@ var templateTwo = React.createElement(
     "button",
     { onClick: addOne },
     "+1"
+  ),
+  React.createElement(
+    "button",
+    { onClick: minusOne },
+    "-1"
+  ),
+  React.createElement(
+    "button",
+    { onClick: reset },
+    "reset"
   )
 );
-console.log(templateTwo);
 
 ReactDOM.render(templateTwo, appRoot);
--- src/app.js
+++ src/app.js
@@ -31,12 +31,19 @@ function getLocation(location) {
 
 let count = 0;
 const addOne = () => console.log("addOne");
+// Challenge
+// Make button "-1" -setup minusOne function and register - log 'minusOne'
+// Make reset button 'reset' - setup reset function - log 'reset'
+const minusOne = () => console.log("minusOne");
+const reset = () => console.log("reset");
+
 const templateTwo = (
   <div>
     <h1>Count: {count}</h1>
     <button onClick={addOne}>+1</button>
+    <button onClick={minusOne}>-1</button>
+    <button onClick={reset}>reset</button>
   </div>
 );
-console.log(templateTwo);
 
 ReactDOM.render(templateTwo, appRoot);
