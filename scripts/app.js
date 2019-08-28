7f1d28772ef096c506774183b7e6ab1bc6a2ab1a
Benson <tumlivein@gmail.com>
2019-08-28 11:22:39 +0800

    Challenge 1 : add Option

--- scripts/app.js
+++ scripts/app.js
@@ -9,75 +9,92 @@ var app = {
 var onFormSubmit = function onFormSubmit(e) {
   e.preventDefault();
 
-  var elements = e.target.elements;
-  var option = elements.option;
-  var value = option.value;
-
-  console.log(elements);
-  // "option" came from input name
-  console.log(option);
-  console.log(value);
-  console.log(e.target.elements.option.value);
+  var option = e.target.elements.option.value;
 
   if (option) {
     app.options.push(option);
+    render();
     e.target.elements.option.value = "";
   }
 };
 
-var template = React.createElement(
-  React.Fragment,
-  null,
-  React.createElement(
-    "h1",
-    { id: "someid" },
-    app.title
-  ),
-  app.subtitle && React.createElement(
-    "p",
-    null,
-    app.subtitle
-  ),
-  React.createElement(
-    "p",
-    null,
-    app.options.length > 0 ? "Here are your options" : "No options"
-  ),
-  React.createElement(
-    "ol",
-    null,
-    React.createElement(
-      "li",
-      null,
-      "Item one"
-    ),
-    React.createElement(
-      "li",
-      null,
-      "Item two"
-    ),
-    React.createElement(
-      "li",
-      null,
-      "Item three"
-    )
-  ),
-  React.createElement(
-    "p",
-    null,
-    app.options.length
-  ),
-  React.createElement(
-    "form",
-    { onSubmit: onFormSubmit },
-    React.createElement("input", { type: "text", name: "option" }),
-    React.createElement(
-      "button",
-      null,
-      "Add Option"
-    )
-  )
-);
-
 var appRoot = document.getElementById("app");
-ReactDOM.render(template, appRoot);
+
+// Create render function that render the new jsx
+// Call it rightaway
+// Call it after options array added to
+
+var render = function render() {
+  var template = React.createElement(
+    React.Fragment,
+    null,
+    React.createElement(
+      "h1",
+      { id: "someid" },
+      app.title
+    ),
+    app.subtitle && React.createElement(
+      "p",
+      null,
+      app.subtitle
+    ),
+    app.options.length > 0 ? React.createElement(
+      "p",
+      null,
+      React.createElement(
+        "b",
+        null,
+        "Here are your options"
+      )
+    ) : React.createElement(
+      "p",
+      null,
+      "No options"
+    ),
+    React.createElement(
+      "ol",
+      null,
+      React.createElement(
+        "li",
+        null,
+        "Item one"
+      ),
+      React.createElement(
+        "li",
+        null,
+        "Item two"
+      ),
+      React.createElement(
+        "li",
+        null,
+        "Item three"
+      ),
+      app.options.map(function (option) {
+        return React.createElement(
+          "li",
+          { key: option },
+          option
+        );
+      })
+    ),
+    React.createElement(
+      "p",
+      null,
+      app.options.length
+    ),
+    React.createElement(
+      "form",
+      { onSubmit: onFormSubmit },
+      React.createElement("input", { type: "text", name: "option" }),
+      React.createElement(
+        "button",
+        null,
+        "Add Option"
+      )
+    )
+  );
+
+  ReactDOM.render(template, appRoot);
+};
+
+render();
--- src/app.js
+++ src/app.js
@@ -7,38 +7,50 @@ const app = {
 const onFormSubmit = e => {
   e.preventDefault();
 
-  const { elements } = e.target;
-  const { option } = elements;
-  const { value } = option;
-  console.log(elements);
-  // "option" came from input name
-  console.log(option);
-  console.log(value);
-  console.log(e.target.elements.option.value);
+  const option = e.target.elements.option.value;
 
   if (option) {
     app.options.push(option);
+    render();
     e.target.elements.option.value = "";
   }
 };
 
-const template = (
-  <React.Fragment>
-    <h1 id="someid">{app.title}</h1>
-    {app.subtitle && <p>{app.subtitle}</p>}
-    <p>{app.options.length > 0 ? "Here are your options" : "No options"}</p>
-    <ol>
-      <li>Item one</li>
-      <li>Item two</li>
-      <li>Item three</li>
-    </ol>
-    <p>{app.options.length}</p>
-    <form onSubmit={onFormSubmit}>
-      <input type="text" name="option" />
-      <button>Add Option</button>
-    </form>
-  </React.Fragment>
-);
-
 const appRoot = document.getElementById("app");
-ReactDOM.render(template, appRoot);
+
+// Create render function that render the new jsx
+// Call it rightaway
+// Call it after options array added to
+
+const render = () => {
+  const template = (
+    <React.Fragment>
+      <h1 id="someid">{app.title}</h1>
+      {app.subtitle && <p>{app.subtitle}</p>}
+      {app.options.length > 0 ? (
+        <p>
+          <b>Here are your options</b>
+        </p>
+      ) : (
+        <p>No options</p>
+      )}
+      <ol>
+        <li>Item one</li>
+        <li>Item two</li>
+        <li>Item three</li>
+        {app.options.map(option => (
+          <li key={option}>{option}</li>
+        ))}
+      </ol>
+      <p>{app.options.length}</p>
+      <form onSubmit={onFormSubmit}>
+        <input type="text" name="option" />
+        <button>Add Option</button>
+      </form>
+    </React.Fragment>
+  );
+
+  ReactDOM.render(template, appRoot);
+};
+
+render();
