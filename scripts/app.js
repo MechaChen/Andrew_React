0d4455770ef702654441dcaf5651746f08ed7e5b
Benson <tumlivein@gmail.com>
2019-08-30 00:50:01 +0800

    34. Alternative setState Syntax: New Fashion prevent setState() problem

--- scripts/app.js
+++ scripts/app.js
@@ -51,20 +51,15 @@ var Counter = function (_React$Component) {
   }, {
     key: "handleReset",
     value: function handleReset() {
-      // this.setState(() => {
-      //   return {
-      //     count: 0
-      //   };
-      // });
-
-      // Old Fashion
-      // work in Asynchronous way
-      // this make this.state.count still in old state, not 0
-      this.setState({
-        count: 0
+      this.setState(function () {
+        return {
+          count: 0
+        };
       });
-      this.setState({
-        count: this.state.count + 1
+      this.setState(function (prevState) {
+        return {
+          count: prevState.count + 1
+        };
       });
     }
   }, {
--- src/playground/counter-example.js
+++ src/playground/counter-example.js
@@ -28,20 +28,15 @@ class Counter extends React.Component {
     });
   }
   handleReset() {
-    // this.setState(() => {
-    //   return {
-    //     count: 0
-    //   };
-    // });
-
-    // Old Fashion
-    // work in Asynchronous way
-    // this make this.state.count still in old state, not 0
-    this.setState({
-      count: 0
+    this.setState(() => {
+      return {
+        count: 0
+      };
     });
-    this.setState({
-      count: this.state.count + 1
+    this.setState(prevState => {
+      return {
+        count: prevState.count + 1
+      };
     });
   }
   render() {
