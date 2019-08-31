128cf277c8e7e672648a48caf6ed71b58a6bcd45
Benson <tumlivein@gmail.com>
2019-08-31 22:31:42 +0800

    45. Saving and Loading Options Data: show some tips when there is no option

--- scripts/app.js
+++ scripts/app.js
@@ -172,6 +172,11 @@ var Options = function Options(props) {
       { onClick: props.handleDeleteOptions },
       "Remove All"
     ),
+    props.options.length === 0 && React.createElement(
+      "p",
+      null,
+      "Please add an option to get started"
+    ),
     props.options.map(function (option) {
       return React.createElement(Option, {
         key: option,
--- src/app.js
+++ src/app.js
@@ -109,6 +109,7 @@ const Options = props => {
   return (
     <div>
       <button onClick={props.handleDeleteOptions}>Remove All</button>
+      {props.options.length === 0 && <p>Please add an option to get started</p>}
       {props.options.map(option => (
         <Option
           key={option}
