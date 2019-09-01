03b6417cedbff34378b0306a859ca26a15695139
Benson <tumlivein@gmail.com>
2019-09-01 23:56:03 +0800

    52. Default Exports: Default Export Reference Error

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,11 @@
 console.log("utils.js is running");
 
-export const square = x => x * x;
+const square = x => x * x;
 
-export const add = (a, b) => a + b;
+const add = (a, b) => a + b;
 
-// export { square, add };
+const substract = (a, b) => a - b;
+
+export { square, add, substract as default };
+
+// export - default export - named exports
