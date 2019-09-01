1fe7ffc612b75874d19cb723f9a127fdc25e3399
Benson <tumlivein@gmail.com>
2019-09-01 22:59:19 +0800

    51. ES6 import/export: export & import square function

--- public/bundle.js
+++ public/bundle.js
@@ -70,20 +70,23 @@
 "use strict";
 Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__utils__ = __webpack_require__(1);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__utils___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0__utils__);
 
 
 console.log("app.js is running!");
-console.log(square(4));
+console.log(__WEBPACK_IMPORTED_MODULE_0__utils__["a" /* square */](4));
 
 
 /***/ }),
 /* 1 */
-/***/ (function(module, exports) {
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
 
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return square; });
 console.log("utils.js is running");
 
 const square = x => x * x;
+
+
 
 
 /***/ })
