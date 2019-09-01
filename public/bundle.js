273cfff456d023ff8f8c3505f3faaea2d64b94f0
Benson <tumlivein@gmail.com>
2019-09-01 22:52:06 +0800

    51. ES6 import/export: First Import

--- public/bundle.js
+++ public/bundle.js
@@ -65,9 +65,22 @@
 /************************************************************************/
 /******/ ([
 /* 0 */
-/***/ (function(module, exports) {
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__utils__ = __webpack_require__(1);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__utils___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0__utils__);
+
 
 console.log("app.js is running!");
+
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports) {
+
+console.log("utils.js is running");
 
 
 /***/ })
