6023b862a16044778320f5b1a0c8dfb101193690
Benson <tumlivein@gmail.com>
2019-09-02 20:55:34 +0800

    53. Importing npm Modules

--- src/app.js
+++ src/app.js
@@ -1,15 +1,6 @@
-// import anythnigIWant, { square, add } from "./utils";
+// install -> import -> use
+import React from "react";
+import ReactDOM from "react-dom";
 
-// console.log("app.js is running!");
-// console.log(square(4));
-// console.log(add(100, 23));
-// console.log(anythnigIWant(100, 81));
-
-import isSenior, { isAdult, canDrink } from "./person";
-
-console.log(isAdult(18));
-console.log(canDrink(18));
-console.log(isSenior(64));
-
-// Setting the default export and function
-// Grab the default and call it
+const template = React.createElement("p", {}, "testing 123");
+ReactDOM.render(template, document.getElementById("app"));
