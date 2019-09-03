65279e5d49d5dfb548be80e366ad14397e48324f
Benson <tumlivein@gmail.com>
2019-09-03 22:18:32 +0800

    60. Passing Children to Component: self-closing Component with Props Implement JSX

--- src/app.js
+++ src/app.js
@@ -2,37 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom";
 import IndecisionApp from "./components/IndecisionApp";
 
-// Moving the component code
-// Setup import/export
-// Move the imports to that new files (and setup an import here)
+const Layout = props => {
+  return (
+    <div>
+      <p>header</p>
+      {props.content}
+      <p>footer</p>
+    </div>
+  );
+};
 
-// Create the file, copy the copy, setup import/export
+const template = (
+  <div>
+    <h1>Page Title</h1>
+    <p>This is my page</p>
+  </div>
+);
 
-ReactDOM.render(<IndecisionApp />, document.getElementById("app"));
-
-class OldSyntax {
-  constructor() {
-    this.name = "Mike";
-    this.getGreeting = this.getGreeting.bind(this);
-  }
-  getGreeting() {
-    return `Hi. My name is ${this.name}.`;
-  }
-}
-const oldSyntax = new OldSyntax();
-const getGreeting = oldSyntax.getGreeting;
-console.log(oldSyntax);
-console.log(getGreeting());
-
-// -----------------------
-
-class NewSyntax {
-  name = "Jen";
-  getGreeting = () => {
-    return `Hi. My name is ${this.name}`;
-  };
-}
-const newSyntax = new NewSyntax();
-const newGreeting = newSyntax.getGreeting;
-console.log(newSyntax);
-console.log(newGreeting());
+ReactDOM.render(<Layout content={template} />, document.getElementById("app"));
