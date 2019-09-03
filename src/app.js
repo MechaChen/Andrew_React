f8c4938da886c3c4e686bea59ef126695a2d9cbe
Benson Chen <tumlivein@gmail.com>
2019-09-03 11:17:21 +0800

    Challenge: make IndecisionApp as a component

--- src/app.js
+++ src/app.js
@@ -1,89 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Header from "./components/Header";
-import Action from "./components/Action";
-import Options from "./components/Options";
-import AddOption from "./components/AddOption";
+import IndecisionApp from "./components/IndecisionApp";
 
-class IndecisionApp extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleDeleteOptions = this.handleDeleteOptions.bind(this);
-    this.handleDeleteOption = this.handleDeleteOption.bind(this);
-    this.handlePick = this.handlePick.bind(this);
-    this.handleAddOption = this.handleAddOption.bind(this);
-    this.state = {
-      options: []
-    };
-  }
-  componentDidMount() {
-    // avoid some bad data, like ['12'] into localStorage
-    try {
-      const json = localStorage.getItem("options");
-      const options = JSON.parse(json);
-
-      // if use localStorage.clear(), localStorage will be null
-      // options.length will fail
-      if (options) {
-        this.setState(() => ({ options }));
-      }
-    } catch (e) {
-      // Do nothing at all
-    }
-  }
-  // fire when props or state change
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.options.length !== this.state.options.length) {
-      const json = JSON.stringify(this.state.options);
-      localStorage.setItem("options", json);
-    }
-  }
-  // fire when your component goes away
-  componentWillUnmount() {
-    console.log("componentWillUnmount");
-  }
-  handleDeleteOptions() {
-    this.setState(() => ({ options: [] }));
-  }
-  handleDeleteOption(optionToRemove) {
-    this.setState(prevState => ({
-      options: prevState.options.filter(option => option !== optionToRemove)
-    }));
-  }
-  handlePick() {
-    const randomNum = Math.floor(Math.random() * this.state.options.length);
-    const option = this.state.options[randomNum];
-    alert(option);
-  }
-  handleAddOption(option) {
-    if (!option) {
-      return "Enter some value.";
-    } else if (this.state.options.indexOf(option) > -1) {
-      return "This value have already existed.";
-    }
-
-    this.setState(prevState => ({ options: prevState.options.concat(option) }));
-  }
-  render() {
-    const subtitle = "Put your life in the hands of a computer";
-
-    return (
-      <div>
-        <Header subtitle={subtitle} />
-        <Action
-          hasOptions={this.state.options.length > 0}
-          handlePick={this.handlePick}
-        />
-        <Options
-          options={this.state.options}
-          handleDeleteOptions={this.handleDeleteOptions}
-          handleDeleteOption={this.handleDeleteOption}
-        />
-        <AddOption handleAddOption={this.handleAddOption} />
-      </div>
-    );
-  }
-}
+// Moving the component code
+// Setup import/export
+// Move the imports to that new files (and setup an import here)
 
 // Create the file, copy the copy, setup import/export
 
